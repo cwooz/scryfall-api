d5362695d6a5a4993dcc38cf962ef29e66a18916
cwooz <woosley.christopher@gmail.com>
2019-08-14 17:21:42 -0400

    'update'

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -8,15 +8,18 @@ export default class Search {
 
   async getResults() {
     // fix for CORS error (cross-browser call)
+    const url = 'https://api.scryfall.com';
     const proxy = 'https://crossorigin.me/';
     const key = 'e7ad8e1dcb4babf119f92615c3d9faf1';
+    const string = '/sets/';
   
     try {
       // axios over 'fetch' call for wider browser compatibility and 1 step process
       // AJAX call w/ Axios, returns promise in json format
-      const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
-      this.result = res.data.recipes;      
-      console.log(this.result);
+      // const res = await axios(`${url}search?key=${key}&q=${this.query}`);
+      const res = await axios(`${url}${string}${this.query}`);
+      // this.result = res.data.recipes;      
+      // console.log(this.result);
       
     } catch (error) {
       console.log(error);
